967fde54491701c752c35c9a8ec64068f7c19cbf
Revathi <revathi.mukkamala96@gmail.com>
2020-10-30 21:02:34 -0700

    adding audio files and english_alphabet_morse python file

--- dot-dash.py
+++ dot-dash.py
@@ -7,11 +7,13 @@ from glob import glob
 dot_time = 100
 dash_time = 300
 
+path_store = './dots_and_dashes/'
+
 print("converting .wav files to dot and dashes!")
 
-playlist_songs = [AudioSegment.from_wav(wav) for wav in glob("/Users/tanmay.ghai/usc/csci566/audios/*")]
+playlist_songs = [AudioSegment.from_wav(wav) for wav in glob("./processed audios/*")]
 
-audio_mediums = glob("/Users/tanmay.ghai/usc/csci566/audios/*")
+audio_mediums = glob("./processed audios/*")
 
 print(audio_mediums)
 
@@ -26,7 +28,7 @@ for song in playlist_songs:
 
 	name = audio_mediums[idx].split("/")[-1].split(".")[0]
 
-	dot.export(name + "-dot.wav", format="wav")
-	dash.export(name + "-dash.wav", format="wav")
+	dot.export(path_store + name + "-dot.wav", format="wav")
+	dash.export(path_store + name + "-dash.wav", format="wav")
 
 	idx+=1
---
+++ english_alphabet_morse.py
@@ -0,0 +1,43 @@
+import json
+import os
+from pydub import AudioSegment
+from pydub.playback import play
+
+one_unit = 0.5
+time_per_unit = 1000
+sleep_time_symbols = time_per_unit * one_unit #sleep time between symbols(dots and dashes) in a character
+
+path_store = "./morse_sound_files/" #path to store generated audio files
+path_get = "./dots_and_dashes/" #path to get dot and dash audio files
+
+def main():
+    with open('./morse_code.json', "r", encoding='utf-8') as file:
+        morse_code = json.load(file)
+
+    #read audio files
+    audio_mediums = []
+    for _,_,files in os.walk("./processed audios"):
+        audio_mediums = [file.split('.')[0] for file in files]
+    #audio mediums
+    print(audio_mediums)
+    #generate alphabet audio files
+    one_unit_sleep = AudioSegment.silent(duration=sleep_time_symbols)
+    for medium in audio_mediums:
+        print("Generating audio file for medium:", medium)
+        dot =  AudioSegment.from_wav(path_get + medium + "-dot.wav")
+        dash = AudioSegment.from_wav(path_get + medium + '-dash.wav')
+        for character, code in morse_code.items():
+            print("character: ", character)
+            character_audio = AudioSegment.silent(duration=0)
+            for c in code:
+                if c == '.':
+                    character_audio += dot + one_unit_sleep
+                elif c == '-':
+                    character_audio += dash + one_unit_sleep
+            #save character audio file
+            character_audio.export(path_store + medium + "/" + character + ".wav", format="wav")
+            play(character_audio)
+
+
+if __name__ == "__main__":
+    main()
